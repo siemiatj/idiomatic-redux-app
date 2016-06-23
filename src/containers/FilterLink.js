bc8e8ee0e116b8bef5926fbba8a77a556ddc392e
siemiatj <kuba@saucelabs.com>
2016-06-23 16:34:17 +0200

    - navigating with react-router

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,20 +1,16 @@
-import { connect } from 'react-redux';
-import { setVisibilityFilter } from '../actions';
-import Link from '../components/Link';
+import React from 'react';
+import { Link } from 'react-router';
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
-});
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick() {
-    dispatch(setVisibilityFilter(ownProps.filter));
-  }
-});
-
-const FilterLink = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link);
+const FilterLink = ({ filter, children }) => (
+  <Link
+    to={filter === 'all' ? '' : filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black'
+    }}
+  >
+    {children}
+  </Link>
+);
 
 export default FilterLink;
