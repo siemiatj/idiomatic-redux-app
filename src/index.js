68df6aacaa165a572fc5bf9037d951d9ae2f7632
siemiatj <kuba@saucelabs.com>
2016-06-23 15:11:38 +0200

    - lesson 3

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
-const persistedState = {
-  todos: [{
-    id: '0',
-    text: 'Welcome back!',
-    completed: false
-  }]
-};
+const persistedState = loadState();
 const store = createStore(
   todoApp,
   persistedState
 );
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+}, 1000));
 
 render(
   <Provider store={store}>
