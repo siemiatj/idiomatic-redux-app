83792d4adf182f2db2dc591e34cd2e693521ac6c
siemiatj <kuba@saucelabs.com>
2016-06-24 16:17:24 +0200

    - lesson 17

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,17 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
+const logger = (store) => (next) => {
   /* eslint-disable no-console */
-  const rawDispatch = store.dispatch;
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
 
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state', 'color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue;
@@ -20,24 +19,30 @@ const addLoggingToDispatch = (store) => {
   /* eslint-enable no-console */
 };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch;
-  };
+/* eslint-disable no-unused-vars */
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next;
+};
+/* eslint-enable no-unused-vars */
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => 
+    store.dispatch = middleware(store)(store.dispatch)
+  );
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [ promise ];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
