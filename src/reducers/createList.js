fc79aadf12f1d69a1598d90f8f15cf23f7827784
siemiatj <kuba@saucelabs.com>
2016-06-28 12:51:31 +0200

    - step 27

--- src/reducers/createList.js
+++ src/reducers/createList.js
@@ -1,6 +1,19 @@
 import { combineReducers } from 'redux';
 
 const createList = (filter) => {
+  const handleToggle = (state, action) => {
+    const { result: toggleId, entities } = action.response;
+    const { completed } = entities.todos[toggleId];
+    const shouldRemove = (
+      (completed && filter === 'active') ||
+      (!completed && filter === 'completed')
+    );
+
+    return shouldRemove ?
+      state.filter(id => id !== toggleId) :
+      state;
+  };
+
   const ids = (state = [], action) => {
     switch (action.type) {
       case 'FETCH_TODOS_SUCCESS':
@@ -11,6 +24,8 @@ const createList = (filter) => {
         return filter !== 'completed' ?
           [...state, action.response.result] :
           state;
+      case 'TOGGLE_TODO_SUCCESS':
+        return handleToggle(state, action);
       default:
         return state;
     }
