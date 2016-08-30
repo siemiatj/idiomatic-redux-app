f27c59422790cd67e3d44dfa621616e8f377bc70
siemiatj <kuba@saucelabs.com>
2016-08-30 14:13:42 +0200

    - add devtools - cleanup - tern config

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
+import DevTools from './Dev/DevTools';
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+    {
+      (() => {
+        if (process.env.NODE_ENV !== 'production') {
+          return <DevTools />;
+        }
+        return null;
+      })()
+    }
   </div>
 );
 
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import todoApp from './reducers';
+import DevTools from './components/Dev/DevTools';
 
 const configureStore = () => {
   const middlewares = [ thunk ];
+  let enhancer;
+
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
+    enhancer = compose(
+      applyMiddleware(...middlewares),
+      // Required! Enable Redux DevTools with the monitors you chose
+      DevTools.instrument()
+    );
+  } else {
+    enhancer = applyMiddleware(...middlewares);
   }
 
   return createStore(
     todoApp,
-    applyMiddleware(...middlewares)
+    enhancer
   );
 };
 
